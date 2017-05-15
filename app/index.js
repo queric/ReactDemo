250662bb89b1707b9167d634d779bea85ef5aef7
unknown <>
2017-05-15 18:55:51 +0800

    fff

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import ProductBox from './Components/ProductBox.js';
 import ListView from './Components/ListView.js';
 import Hello from './Components/Hello';
@@ -15,9 +14,10 @@ ReactDOM.render(<ProductBox title={data}><span>acadmics</span><span>activist</sp
     , document.getElementById('content'));
 ReactDOM.render(
     <ListView onClick={listViewClick}></ListView>
-    , document.getElementById('content2'));*/
-//ReactDOM.render(<Hello/>,document.getElementById("content"))*/
+    , document.getElementById('content2'));
+//ReactDOM.render(<Hello/>,document.getElementById("content"))
 ReactDOM.render(<ListView><a href="dd">6666</a> <a href="dd">hhhhh</a></ListView>,document.getElementById("content"))
+*/
 import {BrowserRouter,Route} from 'react-router-dom';
 import ProductBox from './Components/ProductBox.js';
 import ListView from './Components/ListView.js';
